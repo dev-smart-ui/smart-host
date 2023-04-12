6d927ea6b98d174bdd8ad7af392d96b9e49bbaa1
dev-smart-ui <dev@smart-ui.pro>
2023-04-12 11:55:57 +0400

    feat: add income calculate business logic

---
+++ src/core/calculateIncome.ts
@@ -0,0 +1,34 @@
+const PREMIUM_ROOM_PRICE = 100;
+
+export const calculateIncome = (guestPayments: number[], premiumRoomsCount: number, economyRoomsCount: number) => {
+    guestPayments.sort((a,b) => a - b).reverse();
+
+    const {premiumRooms, economyRooms} = guestPayments.reduce((acc, guestBudget) => {
+        const reservedEconomy = acc.economyRooms.length;
+        const reservedPremium = acc.premiumRooms.length;
+        if(guestBudget < PREMIUM_ROOM_PRICE && reservedEconomy < economyRoomsCount)
+            return {...acc, economyRooms: [...acc.economyRooms, guestBudget]};
+
+        else if(guestBudget >= PREMIUM_ROOM_PRICE && reservedPremium < premiumRoomsCount)
+            return {...acc, premiumRooms: [...acc.premiumRooms, guestBudget]};
+
+        else if(reservedPremium < premiumRoomsCount)
+        {
+            const highestPayment = acc.economyRooms.shift();
+        
+            if(highestPayment) {
+                return {
+                    premiumRooms: [...acc.premiumRooms, highestPayment],
+                    economyRooms: [...acc.economyRooms, guestBudget]
+                };
+            }
+        }
+
+        return acc;
+    }, {premiumRooms: [] as number[], economyRooms: [] as number[]});
+
+    return {
+        economyIncome: premiumRooms.reduce((acc, room) => acc + room, 0), 
+        premiumIncome: economyRooms.reduce((acc, room) => acc + room, 0)
+    };
+};
