babf6d8b043ee4308a4ef4b8c5e1e34a4bd3a06e
dev-smart-ui <dev@smart-ui.pro>
2023-04-12 22:45:12 +0400

    feat: add calculate results hook

---
+++ src/hooks/useCalculateIncome.tsx
@@ -0,0 +1,32 @@
+import { useCallback, useState } from "react";
+import { calculateIncome } from "../core/calculateIncome";
+import { getGuests } from "../services/getGuests";
+
+export const useCalculateIncome = (premiumCount: number, economyCount: number) => {
+    const [income, setIncome] = useState<null | {economyIncome: number; premiumIncome: number}>(null);
+    const [loading, setLoading] = useState(false);
+
+    const onSubmit = useCallback(async () => {
+        try {
+            setLoading(true);
+
+            const guestBudget = await getGuests();
+
+            const income = calculateIncome(guestBudget, premiumCount, economyCount);
+
+            setIncome(income);
+        } catch(err) {
+            console.log(err);
+        } finally {
+            setLoading(false);
+        }
+    }, [premiumCount, economyCount]);
+
+    return {
+        onSubmit,
+        premiumIncome: income?.premiumIncome, 
+        economyIncome: income?.economyIncome, 
+        totalIncome: income ? income?.economyIncome + income?.premiumIncome : null,
+        isLoading: loading
+    };
+};
